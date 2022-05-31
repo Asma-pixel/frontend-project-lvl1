08d71da32c8bad4c4b8faa1813835136593b858c
seraphim <serafimcomru91@gmail.com>
2022-05-31 18:33:41 +0300

    test commit

---
+++ src/cli.js
@@ -0,0 +1,14 @@
+import readlineSync from 'readline-sync';
+
+const getName = () => {
+  const name = readlineSync.question('May i get yur name?\n');
+  return name;
+};
+
+const usrGreeting = () => {
+  const name = getName();
+  const greeting = `Hello, ${name}!`;
+  return greeting;
+};
+
+export default usrGreeting;
